8afe6aed64a3c78de4402b6ee675e4ce0f53f3c5
yummy1gay <artem.okunkov228@gmail.com>
2025-02-15 18:46:23 +0100

    .

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -255,7 +255,7 @@ class Tapper:
         }
 
         try:
-            resp = self.scraper.post(f"{self.url}/user/sync/data", json=data)
+            resp = self.scraper.get(f"{self.url}/user/sync", json=data)
             
             resp.raise_for_status()
 
